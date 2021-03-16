354dbdb2c2c76c9135c419f9c619e416d195444e
Andrey Baranov <andrey66r@yandex.ru>
2021-03-16 21:25:40 +0300

    Update requestValidation.js

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -41,8 +41,8 @@ const validateUser = celebrate({
 
 const validateUserUpdate = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 });
 
